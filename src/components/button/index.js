9a1f2103f1300a2c57c2b979cb42a75eb786deb3
tamicristina <tami.cristina06@gmail.com>
2021-07-30 21:49:44 -0300

    chore: WIP component button

---
+++ src/components/button/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ButtonEnter = () => {
+  return (
+    <div>
+      <button type="button">Entrar</button>
+    </div>
+  );
+};
+
+export default ButtonEnter;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import ButtonEnter from "../../components/button";
+
 const Home = () => {
-  return <div>home works</div>;
+  return (
+    <div>
+      Home Works
+      <ButtonEnter />
+    </div>
+  );
 };
 
 export default Home;
