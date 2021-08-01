ce9858fcfc2c3c25aec910fcafab516105c95170
tamicristina <tami.cristina06@gmail.com>
2021-08-01 13:56:32 -0300

    stylizing button component

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Button } from "../styles";
 
 const ButtonEnter = () => {
   return (
     <div>
-      <button type="button">Entrar</button>
+      {/* <button type="button">Entrar</button> */}
+      <Button>Entrar</Button>
     </div>
   );
 };
---
+++ src/components/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  width: 309px;
+  height: 48px;
+  background-color: #007067;
+  color: #fff9f9;
+  border-radius: 10px;
+  padding: 13px 18.6px 14px 18.6px;
+  box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.18);
+  border: none;
+`;
