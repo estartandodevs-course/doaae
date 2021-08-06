85f0d351eedcb02b74a8809eae6bb6a3c671b37b
Pedro Sandes <pedrosandes122@gmail.com>
2021-08-05 22:18:58 -0300

    chore: SpalshScreen styled alterado

--- src/pages/SplashScreen/styles.js
+++ src/pages/SplashScreen/styles.js
@@ -1,51 +1,34 @@
 import styled from "styled-components";
 
-const Container = styled.main`
+export const Container = styled.main`
   min-height: 100vh;
   font-family: "Roboto";
   display: flex;
   flex-direction: column;
-
-  .container-info {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 33px;
-
-    .info {
-      display: flex;
-      width: 285px;
-      height: 70px;
-      align-items: center;
-      gap: 17px;
-
-      .map-icon {
-        width: 39px;
-        height: 50px;
-      }
-
-      .box-icon {
-        width: 42px;
-        height: 45px;
-      }
-    }
-  }
-
-  .footer {
-    text-align: center;
-    font-size: 11px;
-    color: #666;
-    padding: 17px 0px;
-  }
 `;
 
-const HeaderBackground = styled.header`
+export const ContainerInfo = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  .logo {
-    margin-top: -56px;
-  }
+  margin-top: 33px;
+  margin-bottom: 25px;
+  gap: 15px;
 `;
-export { Container, HeaderBackground };
+
+export const HeaderBackground = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Logo = styled.img`
+  margin-top: -56px;
+`;
+
+export const Footer = styled.footer`
+  text-align: center;
+  font-size: 11px;
+  color: #666;
+  padding: 17px 0px;
+`;
