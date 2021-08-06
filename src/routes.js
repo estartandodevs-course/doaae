4311e039319b9cee8228858b8c9352add0c2c3bb
Daniele <daniszcode>
2021-08-06 12:23:03 -0300

    feat: Component DividerPag com estilizacao

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { Login, SplashScreen, Cadastro } from "./pages";
+import { DividerPage } from "./components/DividerPage";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={SplashScreen} />
-        <Route path="/login" component={Login} />
-        <Route path="/cadastro" component={Cadastro} />
+        <Route exact path="/" component={DividerPage} />
       </Switch>
     </BrowserRouter>
   );
