90b7d7442506db834db8a959f775e3489a0ccbe7
Daniele <daniszcode>
2021-08-10 16:26:45 -0300

    chore: ajustando componente ButtonOpen

--- src/components/ButtonOpenModal/index.js
+++ src/components/ButtonOpenModal/index.js
@@ -1,7 +1,7 @@
 import * as S from "./styles";
 
 const ButtonOpenModal = () => {
-  return <S.ButtonOpen>sem limites</S.ButtonOpen>;
+  return <S.ButtonOpen>sem limites ▼</S.ButtonOpen>;
 };
 
 export { ButtonOpenModal };
