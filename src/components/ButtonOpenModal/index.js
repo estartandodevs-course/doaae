7a9c5b8cebccd2078258c5e2bf715da95e58da09
Daniele <daniszcode>
2021-08-10 14:46:37 -0300

    feat: ButtonOpenModal feito

---
+++ src/components/ButtonOpenModal/index.js
@@ -0,0 +1,7 @@
+import * as S from "./styles";
+
+const ButtonOpenModal = () => {
+  return <S.ButtonOpen>sem limites</S.ButtonOpen>;
+};
+
+export { ButtonOpenModal };
---
+++ src/components/ButtonOpenModal/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const ButtonOpen = styled.button`
+  width: 200px;
+  height: 35px;
+  background-color: #ebebeb;
+  color: #3c3c3c;
+  border-radius: 48px;
+  padding: 8px 93px 6px 14px;
+  border: none;
+  font-family: "Roboto";
+  font-weight: 400;
+  cursor: pointer;
+`;
+
+export { ButtonOpen };
