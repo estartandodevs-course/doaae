05d41a3285942e4f2983f3414a0e97c58027d3a6
Pedro Sandes <pedrosandes122@gmail.com>
2021-08-14 18:34:48 -0300

    chore: WIP Componente cardInstituicao iniciado

---
+++ src/components/CardInstituicao/index.js
@@ -0,0 +1,35 @@
+import * as S from "./styles";
+
+const CardInstituicao = ({
+  avatar,
+  name,
+  categorias,
+  logradouro,
+  numero,
+  bairro,
+  cidade,
+}) => {
+  return (
+    <S.Container>
+      <S.ContainerAvatar>
+        <S.Avatar
+          src={`data:image/png;base64,${avatar}`}
+          alt="Ong profile avatar"
+        />
+      </S.ContainerAvatar>
+      <S.ContainerInfo>
+        <S.Name>{name}</S.Name>
+        <S.CategorysList>
+          {categorias.map((categoria) => (
+            <S.Categorys>{categoria}</S.Categorys>
+          ))}
+        </S.CategorysList>
+        <S.Location>
+          {logradouro},{numero},{bairro}.{cidade}
+        </S.Location>
+      </S.ContainerInfo>
+    </S.Container>
+  );
+};
+
+export { CardInstituicao };
---
+++ src/components/CardInstituicao/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const ContainerInfo = styled.div``;
+
+export const ContainerAvatar = styled.div``;
+
+export const Avatar = styled.img``;
+
+export const Name = styled.h1``;
+
+export const CategorysList = styled.ul``;
+
+export const Categorys = styled.li``;
+
+export const Location = styled.p``;
