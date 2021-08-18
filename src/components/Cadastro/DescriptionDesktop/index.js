9d05c0212555ab15679d2664c4842e7c585fe37e
Pedro Sandes <pedrosandes122@gmail.com>
2021-08-18 17:54:54 -0300

    chore: DescriptionDesktop alterado para ternario

--- src/components/Cadastro/DescriptionDesktop/index.js
+++ src/components/Cadastro/DescriptionDesktop/index.js
@@ -1,25 +1,26 @@
 import * as S from "./styles";
 
 const DescriptionDesktop = ({ small, selected }) => {
-  if (small) {
-    return (
-      <>
-        <S.DescriptionTitle color="#388596">
-          {`Cadastro de ${selected}`}
-        </S.DescriptionTitle>
-        <S.Text margin="36px">
-          Vamos precisar de alguns dados seu para concluir seu cadastro.
-        </S.Text>
-      </>
-    );
-  }
   return (
     <>
-      <S.DescriptionTitle color="#000">Tipo de perfil</S.DescriptionTitle>
-      <S.DescriptionText>
-        <S.Text>De que forma você irá utilizar este aplicativo?</S.Text>
-        <S.Text>Escolha a opção que mais se adequa ao seu perfil.</S.Text>
-      </S.DescriptionText>
+      {small ? (
+        <>
+          <S.DescriptionTitle color="#388596">
+            {`Cadastro de ${selected}`}
+          </S.DescriptionTitle>
+          <S.Text margin="36px">
+            Vamos precisar de alguns dados seu para concluir seu cadastro.
+          </S.Text>
+        </>
+      ) : (
+        <>
+          <S.DescriptionTitle color="#000">Tipo de perfil</S.DescriptionTitle>
+          <S.DescriptionText>
+            <S.Text>De que forma você irá utilizar este aplicativo?</S.Text>
+            <S.Text>Escolha a opção que mais se adequa ao seu perfil.</S.Text>
+          </S.DescriptionText>
+        </>
+      )}
     </>
   );
 };
