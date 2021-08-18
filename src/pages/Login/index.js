0947ca1d46432b2c1b29d187cf6a7b17cc4e8a9d
Pedro Sandes <pedrosandes122@gmail.com>
2021-08-17 23:24:09 -0300

    feat: Reponsividade da pagina login feita, button retirado o ternario

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,46 +2,65 @@ import * as S from "./styles";
 import { Logo } from "../../components/Shared/Logo";
 import { Input, InputPassword } from "../../components/Input";
 import { Button } from "../../components/Button";
+import DesktopImage from "../../assets/img/login-background-desktop.png";
+import LogoEstartando from "../../assets/icons/logo-estartando-devs.svg";
 
 const Login = () => {
   const error = true;
   return (
     <S.Container>
-      <S.Ball />
+      <S.ContainerLogin>
+        <S.BallTop />
 
-      <S.LogoContainer>
-        <Logo />
-      </S.LogoContainer>
-      <S.Form>
-        <S.TitleContainer>
-          <S.Title>Login</S.Title>
-        </S.TitleContainer>
+        <S.ContainerLogo>
+          <Logo />
+        </S.ContainerLogo>
+        <S.Form>
+          <S.ContainerTitle>
+            <S.Title>Login</S.Title>
+          </S.ContainerTitle>
 
-        <S.Inputs>
-          <Input
-            inputType="email"
-            id="email"
-            htmlFor="email"
-            label="Email"
-            errorMessage={null}
-          />
-          <InputPassword
-            id="senha"
-            htmlFor="senha"
-            label="Senha"
-            errorMessage={null}
-          />
-          <S.ForgotPassword error={error}>Esqueci minha senha</S.ForgotPassword>
-        </S.Inputs>
+          <S.Inputs>
+            <Input
+              inputType="email"
+              id="email"
+              htmlFor="email"
+              label="Email"
+              errorMessage={null}
+            />
+            <InputPassword
+              id="senha"
+              htmlFor="senha"
+              label="Senha"
+              errorMessage={null}
+            />
+            <S.ForgotPassword error={error}>
+              Esqueci minha senha
+            </S.ForgotPassword>
+          </S.Inputs>
 
-        <S.OptionsButtons>
-          <Button>Entrar</Button>
+          <S.OptionsButtons>
+            <Button>Entrar</Button>
 
-          <S.CreateAccount>Criar Conta</S.CreateAccount>
+            <S.CreateAccount>Criar Conta</S.CreateAccount>
 
-          <Button color="#389674">Pesquisar Instituições</Button>
-        </S.OptionsButtons>
-      </S.Form>
+            <Button color="#389674">Pesquisar Instituições</Button>
+          </S.OptionsButtons>
+        </S.Form>
+      </S.ContainerLogin>
+
+      <S.ContainerImage>
+        <S.Image src={DesktopImage} />
+        <S.ContainerLogoBottom>
+          <Logo />
+          <S.ContainerLogoEstartando>
+            <S.LogoEstartando src={LogoEstartando} />
+            <S.EstartandoText>Estartando Devs 2021</S.EstartandoText>
+          </S.ContainerLogoEstartando>
+        </S.ContainerLogoBottom>
+
+        <S.BallBottom />
+      </S.ContainerImage>
     </S.Container>
   );
 };
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -2,26 +2,55 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const Container = styled.main`
+  min-height: 100vh;
   padding: 0 30px;
+  overflow: hidden;
+  @media (min-width: 800px) {
+    display: flex;
+    padding: inherit;
+  }
 `;
 
-export const Ball = styled.div`
+export const BallTop = styled.div`
   position: fixed;
   border-radius: 50%;
   top: -497px;
-  left: -110px;
+  left: -100px;
   width: 599px;
   height: 599px;
   background-color: #47c0a1;
   z-index: -1;
+
+  @media (min-width: 468px) {
+    display: none;
+  }
+  @media (min-width: 800px) {
+    display: block;
+    top: -441px;
+    left: -103px;
+  }
 `;
 
-export const LogoContainer = styled.div`
+export const BallBottom = styled.div`
+  position: fixed;
+  width: 790px;
+  height: 580px;
+  background-color: #47c0a1;
+  bottom: -411px;
+  right: -454px;
+  border-radius: 50%;
+`;
+
+export const ContainerLogo = styled.div`
   display: flex;
   justify-content: center;
 
   > img {
     margin-top: 10px;
+
+    @media (min-width: 800px) {
+      margin-top: 40px;
+    }
   }
 `;
 
@@ -31,10 +60,15 @@ export const Form = styled.form`
   align-items: center;
   margin-top: 67px;
   position: relative;
+  padding: 0 30px;
 `;
 
-export const TitleContainer = styled.div`
+export const ContainerTitle = styled.div`
   width: 309px;
+
+  @media (min-width: 800px) {
+    display: none;
+  }
 `;
 
 export const Title = styled.h1`
@@ -78,4 +112,69 @@ export const CreateAccount = styled(Link)`
   text-decoration: underline;
   margin: 8px 0 26px 0;
   text-align: center;
+`;
+
+export const ContainerLogin = styled.div`
+  width: auto;
+
+  @media (min-width: 800px) {
+    width: 400px;
+    padding-top: 25px;
+  }
+`;
+
+export const ContainerImage = styled.div`
+  width: auto;
+  margin-left: auto;
+  object-fit: cover;
+  z-index: -4;
+  position: relative;
+  &::before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    content: "";
+    width: 160px;
+    height: 100vh;
+    background: linear-gradient(
+      90deg,
+      #ffffff 22%,
+      rgba(255, 255, 255, 0) 100%
+    );
+  }
+
+  @media (max-width: 800px) {
+    display: none;
+  }
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+export const ContainerLogoBottom = styled.div`
+  position: fixed;
+  bottom: 5px;
+  right: 0;
+  z-index: 2;
+
+  > img {
+    width: 263px;
+    height: 109px;
+  }
+`;
+
+export const LogoEstartando = styled.img``;
+
+export const ContainerLogoEstartando = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+`;
+
+export const EstartandoText = styled.span`
+  color: #fff;
+  font-size: 14px;
 `;
