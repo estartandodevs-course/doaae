1232721d3c43191a319650a94ca19a404763cb4d
yagovaluchedevs <valuchedevs@gmail.com>
2021-08-19 03:11:25 -0300

    feat: estruturando a pagina home e posicionando os seus elementos em seus respectivos lugares (responsiva +- pronto)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { HeaderBar } from "../../components/HeaderBar";
+import { CardCategory } from "../../components/CardCategory";
+import { dataListCategory } from "../../mock/naveCategories";
+import * as S from "./styles";
 
 const Home = () => {
-  return <p>home work</p>;
+  return (
+    <S.ContainerMasterHome>
+      <HeaderBar />
+      <S.ContainerNavigations>
+        <S.ContainerNavigationCategory>
+          <div>
+            <h1>Navegue pelas categorias de doação</h1>
+          </div>
+          <S.ListCardsByCategory>
+            {dataListCategory.map(({ id, photo, name }) => (
+              <CardCategory
+                key={id}
+                imageCategory={photo}
+                nameCategory={name}
+              />
+            ))}
+          </S.ListCardsByCategory>
+        </S.ContainerNavigationCategory>
+        <section>sdad</section>
+      </S.ContainerNavigations>
+    </S.ContainerMasterHome>
+  );
 };
 
 export default Home;
