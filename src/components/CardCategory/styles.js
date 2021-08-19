8572c063b5997850eb2b6d8e1d8a0381603e8707
yagovaluchedevs <valuchedevs@gmail.com>
2021-08-19 01:22:06 -0300

    style: corrigindo responsividade do componente

--- src/components/CardCategory/styles.js
+++ src/components/CardCategory/styles.js
@@ -4,11 +4,17 @@ export const ContainerCard = styled.section`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
-  min-width: 280px;
   max-width: 300px;
+  min-width: 150px;
   height: 123px;
   border-radius: 10px;
   box-shadow: 0px 4px 16px rgb(0 0 0 / 18%);
+
+  @media (max-width: 540px) {
+    flex-direction: column;
+    width: 150px;
+    padding: 12px 0;
+  }
 `;
 
 export const ImageCategoryCard = styled.img`
@@ -21,4 +27,9 @@ export const NameCategory = styled.span`
   font-weight: bold;
   font-size: 22.73px;
   line-height: 27px;
+
+  @media (max-width: 540px) {
+    font-size: 16px;
+    line-height: 19px;
+  }
 `;
