85c465ad7fd935985cd0ef615e023879c2e2a893
tamicristina <tami.cristina06@gmail.com>
2021-08-18 21:58:06 -0300

    Botão ver todas as ações com estilização

---
+++ src/components/ViewActions/index.js
@@ -0,0 +1,10 @@
+import { ViewActionsStyled } from "./styles";
+
+const ViewActions = () => {
+  return (
+    <ViewActionsStyled>
+      <p>ver todas as ações</p>
+    </ViewActionsStyled>
+  );
+};
+export { ViewActions };
