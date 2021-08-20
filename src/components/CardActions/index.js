9238074d90d27310079f4335424dd45ce35a2b3b
Pedro Sandes <pedrosandes122@gmail.com>
2021-08-19 23:37:21 -0300

    refactor: Refatoração do código

--- src/components/CardActions/index.js
+++ src/components/CardActions/index.js
@@ -1,38 +1,33 @@
+import { useState } from "react";
 import * as S from "./styles";
 
-const CardActions = ({
-  cape,
-  title,
-  description,
-  location,
-  category,
-  isOpen,
-  onClick,
-}) => {
-  if (isOpen) {
-    console.log(isOpen);
-    return (
-      <S.Container onClick={onClick} small={isOpen}>
-        <S.ContainerCape>
-          <S.Cape src={cape} />
-          <S.Category>{category}</S.Category>
-        </S.ContainerCape>
-        <S.ContainerInfo>
-          <S.Title>{title}</S.Title>
-          <S.Description>{description}</S.Description>
-          <S.Location>Endereço</S.Location>
-          <S.LocationInfo>{location}</S.LocationInfo>
-        </S.ContainerInfo>
-      </S.Container>
-    );
-  }
+const CardActions = ({ cape, title, description, location, category }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <S.Container onClick={onClick}>
-      <S.ContainerCape>
-        <S.Cape src={cape} />
-        <S.Category>{category}</S.Category>
-      </S.ContainerCape>
-    </S.Container>
+    <>
+      {isOpen ? (
+        <S.Container onClick={() => setIsOpen(!isOpen)} small={isOpen}>
+          <S.ContainerCape>
+            <S.Cape src={cape} />
+            <S.Category>{category}</S.Category>
+          </S.ContainerCape>
+          <S.ContainerInfo>
+            <S.Title>{title}</S.Title>
+            <S.Description>{description}</S.Description>
+            <S.Location>Endereço</S.Location>
+            <S.LocationInfo>{location}</S.LocationInfo>
+          </S.ContainerInfo>
+        </S.Container>
+      ) : (
+        <S.Container onClick={() => setIsOpen(!isOpen)}>
+          <S.ContainerCape>
+            <S.Cape src={cape} />
+            <S.Category>{category}</S.Category>
+          </S.ContainerCape>
+        </S.Container>
+      )}
+    </>
   );
 };
 
