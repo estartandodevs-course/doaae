00486effb42b637c70e1acc8cc8f091c1e2e4d99
Pedro Sandes <pedrosandes122@gmail.com>
2021-08-20 17:53:50 -0300

    chore: Adicionado ao index de paginas

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import SplashScreen from "./SplashScreen";
 import Login from "./Login";
 import Cadastro from "./Cadastro";
 import AgendarDoacao from "./AgendarDoacao";
+import EditarPerfil from "./EditarPerfil";
 
-export { Home, SplashScreen, Login, Cadastro, AgendarDoacao };
+export { Home, SplashScreen, Login, Cadastro, AgendarDoacao, EditarPerfil };
