89d55fd325a1c903560bb2194547c2a72692f485
yagovaluchedevs <valuchedevs@gmail.com>
2021-08-21 00:44:22 -0300

    feat: adicionando rota para redirecionar para a pagina do mapa

--- src/routes.js
+++ src/routes.js
@@ -7,18 +7,20 @@ import {
   Home,
   AgendarDoacao,
   EditarPerfil,
+  PageMapMobile,
 } from "./pages";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/home" component={Home} />
         <Route exact path="/" component={SplashScreen} />
+        <Route path="/home" component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/cadastro" component={Cadastro} />
         <Route path="/agendardoacao" component={AgendarDoacao} />
         <Route path="/editarperfil" component={EditarPerfil} />
+        <Route path="/mapa" component={PageMapMobile} />
       </Switch>
     </BrowserRouter>
   );
