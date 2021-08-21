c0f1a934f310fb6ff7c98762462999c68a48a111
yagovaluchedevs <valuchedevs@gmail.com>
2021-08-21 00:44:56 -0300

    chore: adicionando importacao pelo index das pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,5 +4,14 @@ import Login from "./Login";
 import Cadastro from "./Cadastro";
 import AgendarDoacao from "./AgendarDoacao";
 import EditarPerfil from "./EditarPerfil";
+import PageMapMobile from "./PageMapMobile";
 
-export { Home, SplashScreen, Login, Cadastro, AgendarDoacao, EditarPerfil };
+export {
+  Home,
+  SplashScreen,
+  Login,
+  Cadastro,
+  AgendarDoacao,
+  EditarPerfil,
+  PageMapMobile,
+};
