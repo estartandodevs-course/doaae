91758c0c078bffa67f99591779651eac33c5f403
yagovaluchedevs <valuchedevs@gmail.com>
2021-08-21 01:02:08 -0300

    chore: passando prop de onClick para executar determinada funcao ao clicar

--- src/components/ButtonMap/index.js
+++ src/components/ButtonMap/index.js
@@ -1,10 +1,10 @@
-import * as S from "./styles";
 import MapIcon from "../../assets/icons/map-icon.svg";
 import NextLink from "../../assets/icons/next-link.svg";
+import * as S from "./styles";
 
-const ButtonMap = () => {
+const ButtonMap = ({ onClick }) => {
   return (
-    <S.Container>
+    <S.Container onClick={onClick}>
       <S.MapIcon src={MapIcon} />
       <S.ContainerText to="mapa">
         <S.Text>Buscar por instituições próximas pelo mapa</S.Text>
