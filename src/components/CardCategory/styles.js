6a72262ce708f0aec62beb0c50f766779b0e58e7
Pedro Sandes <pedrosandes122@gmail.com>
2021-08-25 01:41:28 -0300

    style: Valor do media queries alterado, width alterado para 100%.

--- src/components/CardCategory/styles.js
+++ src/components/CardCategory/styles.js
@@ -5,14 +5,14 @@ export const ContainerCard = styled.section`
   justify-content: space-evenly;
   align-items: center;
   max-width: 300px;
-  width: 250px;
+  width: 100%;
   min-width: 150px;
   height: 123px;
   border-radius: 10px;
   box-shadow: 0px 4px 16px rgb(0 0 0 / 18%);
   margin: 12px 2px;
 
-  @media (max-width: 989px) {
+  @media (max-width: 1225px) {
     flex-direction: column;
     width: 150px;
     padding: 12px 0;
@@ -30,7 +30,7 @@ export const NameCategory = styled.p`
   font-size: 22.73px;
   line-height: 27px;
 
-  @media (max-width: 989px) {
+  @media (max-width: 1225px) {
     font-size: 16px;
     line-height: 19px;
   }
