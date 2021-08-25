37fef7308b333ddc876a14a146c50c3970aea1b8
Pedro Sandes <pedrosandes122@gmail.com>
2021-08-24 22:12:24 -0300

    feat: Adicionado funcionalidade de pegar os dados da instituição capturado por id e exbindo na tela

--- src/pages/InstitutionalData/index.js
+++ src/pages/InstitutionalData/index.js
@@ -1,49 +1,51 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { instituicoes } from "../../mock/ongs";
 import { CardActions } from "../../components/CardActions";
+import Cape from "../../assets/img/cape.png";
 import { HeaderBar } from "../../components/HeaderBar";
 import { ViewActions } from "../../components/ViewActions";
 import { CardToShedule } from "../../components/CardToShedule";
 import Whatsapp from "../../assets/img/whatsapp.png";
 import emailImage from "../../assets/img/email.png";
-import foneImage from "../../assets/img/whatsapp.png";
+import foneImage from "../../assets/img/fone.png";
 import actions from "../../mock/actionsData";
 import * as S from "./styles";
 
-const InstitutionalData = ({
-  cape,
-  logo,
-  nome,
-  categorias,
-  logradouro,
-  numero,
-  complemento,
-  estado,
-  telefone,
-  email,
-}) => {
+const InstitutionalData = () => {
+  const { idInst } = useParams();
+
+  const getInstitution = instituicoes.find(({ id }) => id === idInst);
+
+  const { logo, nome, categorias, endereco, telefone, email } = getInstitution;
+
   return (
     <S.SectionOfPage>
       <S.ContainerFromHeader>
-        <HeaderBar isLogged>Instituição</HeaderBar>
+        <HeaderBar isLogged backTo="/home">
+          Instituição
+        </HeaderBar>
       </S.ContainerFromHeader>
-      <S.ImagefromInstitutional bg={cape} />
+      <S.ImagefromInstitutional>
+        <img src={Cape} alt="Capa" />
+      </S.ImagefromInstitutional>
       <S.ContainerInstitutionData>
         <S.InformationFromInstitution>
-          <img src={logo} />
+          <img src={logo} alt={`Logo da instituição ${nome}`} />
           <S.PositionDataInstitution>
             <S.NameAndEvaluatioInstitution>
               <S.NameInstitution>{nome}</S.NameInstitution>
               <S.EvaluationInstitution>4.8</S.EvaluationInstitution>
             </S.NameAndEvaluatioInstitution>
             <S.ContainerCategoriesOfInstitutions>
-              <S.CategoryItems>{categorias}</S.CategoryItems>
+              <S.CategoryItems>{categorias.join(" · ")}</S.CategoryItems>
             </S.ContainerCategoriesOfInstitutions>
           </S.PositionDataInstitution>
         </S.InformationFromInstitution>
         <S.ContainerDescription>
           <div>
             <S.GenericTitle>Endereço</S.GenericTitle>
-            <p>{` ${logradouro}, ${numero}, ${complemento}. ${estado}.`}</p>
+            <p>{` ${endereco.logradouro}, ${endereco.numero}, ${endereco.complemento}. ${endereco.estado}.`}</p>
           </div>
           <S.AboutTheInstitution>
             <S.GenericTitle>Sobre a ONG Mais</S.GenericTitle>
