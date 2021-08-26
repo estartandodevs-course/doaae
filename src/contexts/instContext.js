2c9821e844f04f8caced90dead8e7ecb1cb44e28
Pedro Sandes <pedrosandes122@gmail.com>
2021-08-25 22:11:00 -0300

    feat: Adicionando o contexto das instituições em uma nova file

---
+++ src/contexts/instContext.js
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+import { getAllInstituicoes } from "../services/api";
+
+const InstituicaoContext = createContext({
+  instituicoes: [],
+  setInstituicoes: () => {},
+  getAll: () => {},
+});
+
+const InstituicaoProvider = ({ children }) => {
+  const [instituicoes, setInstituicoes] = useState([]);
+
+  const getAll = () => {
+    const res = getAllInstituicoes();
+    setInstituicoes(res);
+  };
+
+  return (
+    <InstituicaoContext.Provider value={{ instituicoes, getAll }}>
+      {children}
+    </InstituicaoContext.Provider>
+  );
+};
+
+export { InstituicaoContext, InstituicaoProvider };
