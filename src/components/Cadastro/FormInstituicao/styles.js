3db0680253289c77fbb80fc48f9a42f8413c4229
Pedro Sandes <pedrosandes122@gmail.com>
2021-08-27 00:14:07 -0300

    chore: Adicionando formulário apenas para a instituição

---
+++ src/components/Cadastro/FormInstituicao/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+  padding-top: 26px;
+  @media (min-width: 768px) {
+    width: 308px;
+  }
+`;
+
+export const Title = styled.h1`
+  font-family: "Roboto";
+  font-size: 22px;
+  font-weight: 700;
+  margin-bottom: 13px;
+`;
+
+export const ContainerButton = styled.div`
+  width: 100%;
+  @media (min-width: 368px) {
+    width: 308px;
+  }
+`;
