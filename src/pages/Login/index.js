e1a0f29b9ac09c4b6953c51fea0563d7a1a2dc74
yagovaluchedevs <valuchedevs@gmail.com>
2021-08-28 01:49:12 -0300

    feat: implementando validacao do formulario e passando um redirect para o botao

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,30 @@
-import * as S from "./styles";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { useHistory } from "react-router-dom";
 import { Logo } from "../../components/Shared/Logo";
 import { LogoBall } from "../../components/LogoBall";
 import { Input, InputPassword } from "../../components/Input";
 import { Button } from "../../components/Button";
 import DesktopImage from "../../assets/img/login-background-desktop.png";
 import LogoEstartando from "../../assets/icons/logo-estartando-devs.svg";
+import * as S from "./styles";
 
 const Login = () => {
-  const error = true;
+  const history = useHistory();
+
+  const validationOfForm = Yup.object().shape({
+    email: Yup.string()
+      .email("Digite um e-mail válido")
+      .required("Preencha esse campo para continuar"),
+    senha: Yup.string()
+      .min(6, "A senha deve ter no mínimo 6 digítos")
+      .required("Preencha esse campo para continuar"),
+  });
+
+  const redirectToHome = () => {
+    return history.push("/home");
+  };
+
   return (
     <S.Container>
       <S.ContainerLogin>
@@ -17,36 +34,53 @@ const Login = () => {
             <S.Title>Login</S.Title>
           </S.ContainerTitle>
 
-          <S.Inputs>
-            <Input
-              inputType="email"
-              id="email"
-              htmlFor="email"
-              label="Email"
-              errorMessage={null}
-            />
-            <InputPassword
-              id="senha"
-              htmlFor="senha"
-              label="Senha"
-              errorMessage={null}
-            />
-            <S.ForgotPassword error={error}>
-              Esqueci minha senha
-            </S.ForgotPassword>
-          </S.Inputs>
+          <Formik
+            initialValues={{
+              email: "",
+              senha: "",
+            }}
+            validationSchema={validationOfForm}
+          >
+            {({ errors }) => {
+              return (
+                <>
+                  <S.Inputs>
+                    <Input
+                      inputType="email"
+                      id="email"
+                      htmlFor="email"
+                      label="Email"
+                      name="email"
+                    />
 
-          <S.OptionsButtons>
-            <Button to="/home" width="100%">
-              Entrar
-            </Button>
+                    <InputPassword
+                      id="senha"
+                      htmlFor="senha"
+                      label="Senha"
+                      name="senha"
+                    />
+                    <S.ForgotPassword error={errors.senha}>
+                      Esqueci minha senha
+                    </S.ForgotPassword>
+                  </S.Inputs>
 
-            <S.CreateAccount to="/cadastro">Criar Conta</S.CreateAccount>
+                  <S.OptionsButtons>
+                    <S.ButtonForForm onClick={redirectToHome} width="100%">
+                      Entrar
+                    </S.ButtonForForm>
 
-            <Button color="#389674" width="100%">
-              Pesquisar Instituições
-            </Button>
-          </S.OptionsButtons>
+                    <S.CreateAccount to="/cadastro">
+                      Criar Conta
+                    </S.CreateAccount>
+
+                    <Button color="#389674" width="100%">
+                      Pesquisar Instituições
+                    </Button>
+                  </S.OptionsButtons>
+                </>
+              );
+            }}
+          </Formik>
         </S.Form>
       </S.ContainerLogin>
 
