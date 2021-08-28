b089345c6205e099a9b0940ee7ac1c15e971a3a3
yagovaluchedevs <valuchedevs@gmail.com>
2021-08-28 01:46:47 -0300

    style: separando o input default em um componente de estilo

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
+import { Field } from "formik";
 
 const InputElement = styled.div`
-  // Estilização da div que segura o Input e Label
   position: relative;
   width: ${({ width }) => width || "100%"};
   height: 3rem;
@@ -9,7 +9,29 @@ const InputElement = styled.div`
   font-size: 1.125rem;
   margin-bottom: 33px;
 
-  // Mensagem de error
+  .label-form {
+    font-family: inherit;
+    font-weight: 300;
+    color: ${({ error }) => (error ? "#af4242" : "#393939")};
+    font-size: inherit;
+    position: absolute;
+    top: 0.7rem;
+    left: 0.8rem;
+    padding: 0 0.5rem;
+    cursor: text;
+    transition: font-size 0.2s ease, top 0.2s ease, left 0.2s ease;
+  }
+
+  .input-form:focus ~ .label-form,
+  .input-form:not(:placeholder-shown).input-form:not(:focus) ~ .label-form {
+    font-size: 1rem;
+    top: -0.7rem;
+    left: 0.6rem;
+    font-weight: 500;
+    color: ${({ error }) => (error ? "#af4242" : "#389674")};
+    background-color: #fff;
+  }
+
   .input-error-msg {
     display: block;
     font-size: 0.875rem;
@@ -20,58 +42,6 @@ const InputElement = styled.div`
     right: 0px;
   }
 
-  // Estilização do input e label
-  .input-form {
-    position: absolute;
-    top: 0;
-    left: 0;
-    border: 2px solid;
-    border-color: ${({ error }) => (error !== null ? "#af4242" : "#389674")};
-    border-radius: 0.625rem;
-    width: 100%;
-    height: 100%;
-    font-family: inherit;
-    font-size: inherit;
-    font-weight: 500;
-    padding-left: 11px;
-    padding-right: 16px;
-    box-sizing: border-box;
-    outline: none;
-    background: none;
-    transition: border 0.2s ease;
-  }
-
-  .input-form:focus {
-    border: 3px solid;
-    border-color: ${({ error }) => (error !== null ? "#af4242" : "#389674")};
-  }
-
-  .label-form {
-    font-family: inherit;
-    font-weight: 300;
-    color: ${({ error }) => (error !== null ? "#af4242" : "#393939")};
-    font-size: inherit;
-
-    position: absolute;
-    top: 0.7rem;
-    left: 0.8rem;
-    padding: 0 0.5rem;
-    cursor: text;
-    transition: font-size 0.2s ease, top 0.2s ease, left 0.2s ease;
-  }
-
-  // Animação do input
-  .input-form:focus ~ .label-form,
-  .input-form:not(:placeholder-shown).input-form:not(:focus) ~ .label-form {
-    font-size: 1rem;
-    top: -0.7rem;
-    left: 0.6rem;
-    font-weight: 500;
-    color: ${({ error }) => (error !== null ? "#af4242" : "#389674")};
-    background-color: #fff;
-  }
-
-  // Posicionamento do ícone
   .input-icon {
     cursor: pointer;
     position: absolute;
@@ -80,4 +50,28 @@ const InputElement = styled.div`
   }
 `;
 
-export { InputElement };
+const InputDefault = styled(Field)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  border: 2px solid;
+  border-color: ${({ error }) => (error ? "#af4242" : "#389674")};
+  border-radius: 0.625rem;
+  width: 100%;
+  height: 100%;
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: 500;
+  padding-left: 11px;
+  padding-right: 16px;
+  box-sizing: border-box;
+  outline: none;
+  background: none;
+  transition: border 0.2s ease;
+
+  .input-form:focus {
+    border: 3px solid;
+    border-color: ${({ error }) => (error ? "#af4242" : "#389674")};
+  }
+`;
+export { InputElement, InputDefault };
